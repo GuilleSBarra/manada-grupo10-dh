b925a31cfb37d2a26b92b0f5c681b284f0e7ea60
NKATUNSKIS <natalia.katunskis@gmail.com>
2021-08-19 01:54:38 -0300

    mod: home para versión +780 y +1200

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "./public")));
 const publicFolderPath = path.resolve(__dirname,"./public");
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 app.get("/sections", (req, res) => {
     res.sendFile(path.join(__dirname, "./view/sections.html"));
   });
-app.listen(3000,()=>{console.log('Server is runnig in the Port 3000');
+app.listen(port,()=>{console.log(`bServer is runnig in the Port ${port}`);
   });
