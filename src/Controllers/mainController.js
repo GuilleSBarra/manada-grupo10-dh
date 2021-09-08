82ddf56d7f91665f12bb7f2a8932407f44e213c3
daramayo90 <da.aramayo1990@gmail.com>
2021-09-07 21:06:51 -0300

    Sprint 3 - Jira feature: Separate repeating components into partial files

--- src/Controllers/mainController.js
+++ src/Controllers/mainController.js
@@ -1,5 +1,10 @@
+const categories = require('../database/categories.json');
+const salesProducts = require('../database/productsSale.json');
+
 let home = (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', { 
+    salesProducts: salesProducts,
+    categories: categories });
 };
 
 module.exports = { home }
--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -1,9 +1,11 @@
+const products = require('../database/products.json');
+
 let carrito = (req, res) => {
     res.render('./products/cart.ejs');
 };
 
 let shop = (req, res) => {
-    res.render('./products/shop.ejs');
+    res.render('./products/shop.ejs', { products: products });
 };
 
 module.exports = { carrito, shop }
