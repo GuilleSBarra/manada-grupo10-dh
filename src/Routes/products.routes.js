503c07cd5da7ac86d016a716ae65ba2494998656
NKATUNSKIS <natalia.katunskis@gmail.com>
2021-09-19 00:51:20 -0300

    Sprint 4 - CRUD of products - /Products (GET)

--- src/Routes/products.routes.js
+++ src/Routes/products.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('../controllers/productsController');
+const controllerAdmin = require('../Controllers/adminController')
 const router = express.Router();
 
 router.get("/shop", controller.shop);
 router.get("/carrito", controller.carrito);
+
+router.get("/create",controllerAdmin.admin)
 
 module.exports = router;
