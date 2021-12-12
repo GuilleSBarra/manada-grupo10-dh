134660e8e1bdece12e64977c9a0f4cf06f2f8f22
daramayo90 <da.aramayo1990@gmail.com>
2021-12-12 18:57:39 -0500

    test

---
+++ src/Routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./main.routes'));
+router.use('/users', require('./users.routes'));
+router.use('/products', require('./products.routes'))
+router.use('/category', require('./categories.routes'))
+router.use('/admin', require('./admin.routes'));
+
+module.exports = router;
